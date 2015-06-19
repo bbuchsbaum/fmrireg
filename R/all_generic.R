6db8898fdbe987b12f407e8c2f50a4ba7232a26e
bbuchsbaum <brad.buchsbaum@gmail.com>
2015-06-19 16:20:16 -0400

    hhhh

--- R/all_generic.R
+++ R/all_generic.R
@@ -1,29 +1,45 @@
+
+#' @export
 parentTerms <- function(x) UseMethod("parentTerms")
 
+#' @export
 cells <- function(x, ...) UseMethod("cells")
 
+#' @export
 conditions <- function(x, ...) UseMethod("conditions")
 
+#' @export
 convolve <- function(x, hrf, samplingFrame,...) UseMethod("convolve")
 
+#' @export
 isContinuous <- function(x) UseMethod("isContinuous")
 
+#' @export
 levels <- function(x) UseMethod("levels")
 
+#' @export
 columns <- function(x) UseMethod("columns")
 
+#' @export
 designMatrix <- function(x, ...) UseMethod("designMatrix")
 
+#' @export
 elements <- function(x, ...) UseMethod("elements")
 
+#' @export
 evaluate <-  function(x, samplingGrid, ...) UseMethod("evaluate")
 
+#' @export
 nbasis <-  function(x) UseMethod("nbasis")
 
+#' @export
 onsets <-  function(x) UseMethod("onsets")
 
+#' @export
 durations <-  function(x) UseMethod("durations")
 
+#' @export
 amplitudes <-  function(x) UseMethod("amplitudes")
 
+#' @export
 samples <-  function(x, ...) UseMethod("samples")
