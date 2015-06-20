852d8b5091ae7f8c6c09adf3bdf3731924c18e72
bbuchsbaum <brad.buchsbaum@gmail.com>
2015-06-19 21:26:51 -0400

    more docs

--- R/all_generic.R
+++ R/all_generic.R
@@ -5,41 +5,76 @@ parentTerms <- function(x) UseMethod("parentTerms")
 #' @export
 cells <- function(x, ...) UseMethod("cells")
 
+#' conditions
+#' 
+#' return the set of condition labels associated with a model term
+#' @param x the model term
+#'@param ...
 #' @export
 conditions <- function(x, ...) UseMethod("conditions")
 
+#' convolve
+#' 
+#' convolve a term \code{x} with a hemodynamic response over a \code{samplingFrame}
+#' 
 #' @export
+#' @param x a event sequence
+#' @param hrf a hemodynamic response function
+#' @param samplingFrame the time series grid over whcih to sample the fucntion.
+#' @param ...
 convolve <- function(x, hrf, samplingFrame,...) UseMethod("convolve")
 
+#' isContinuous
+#' 
+#' checks to see if a variable is continuous e.g. numeric/non-categorical
 #' @export
 isContinuous <- function(x) UseMethod("isContinuous")
 
+#' levels
+#' 
+#' extract the levels of a term
+#' @param x the term
 #' @export
 levels <- function(x) UseMethod("levels")
 
+#' columns
+#' 
+#' return the column labels associated with the elements of a term.
+#' @param x the term
 #' @export
 columns <- function(x) UseMethod("columns")
 
+#' designMatrix
+#' 
+#' construct a design matrix from the term
+#' @param x the term
 #' @export
 designMatrix <- function(x, ...) UseMethod("designMatrix")
 
+#' elements
 #' @export
 elements <- function(x, ...) UseMethod("elements")
 
+#' evaluate
 #' @export
 evaluate <-  function(x, samplingGrid, ...) UseMethod("evaluate")
 
+#' nbasis
 #' @export
 nbasis <-  function(x) UseMethod("nbasis")
 
+#' onsets
 #' @export
 onsets <-  function(x) UseMethod("onsets")
 
+#' durations
 #' @export
 durations <-  function(x) UseMethod("durations")
 
+#' amplitudes
 #' @export
 amplitudes <-  function(x) UseMethod("amplitudes")
 
+#' samples
 #' @export
 samples <-  function(x, ...) UseMethod("samples")
