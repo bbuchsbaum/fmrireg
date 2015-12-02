557ea29ae2377591df52c41c7c0e09040bc9be6e
bbuchsbaum <brad.buchsbaum@gmail.com>
2015-12-02 07:23:52 -0500

    added fmrireg package doc

---
+++ R/fmrireg.R
@@ -0,0 +1,9 @@
+
+#' fmrireg: regresssion tools for fMRI data
+#'
+#' fmrireg provides functions for generating experimental design matrices appropriate for analyzing fMRI data with regression.
+#'
+#' @docType package
+#' @name fmrireg
+#' @import assertthat
+NULL
