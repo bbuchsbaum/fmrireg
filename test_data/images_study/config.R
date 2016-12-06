e2374dbdb6189748d65c9579c76e1b3b15c425e9
bbuchsbaum <brad.buchsbaum@gmail.com>
2016-12-06 09:48:05 -0500

    hh

--- test_data/images_study/config.R
+++ test_data/images_study/config.R
@@ -3,21 +3,28 @@ base_path="/Users/brad/code/fmrireg/test_data/images_study"
 
 scans = paste0("epi/",
                c("rscan01.nii",
-               "rscan02.nii",
-               "rscan03.nii",
-               "rscan04.nii",
-               "rscan05.nii",
-               "rscan06.nii"))
+                 "rscan02.nii",
+                 "rscan03.nii",
+                 "rscan04.nii",
+                 "rscan05.nii",
+                 "rscan06.nii"))
 
 nuisance_reg = paste0("epi/",
                       c("nuisance_rscan01.txt",
                         "nuisance_rscan02.txt",
                         "nuisance_rscan03.txt",
                         "nuisance_rscan04.txt",
-                        "nuisance_rscan05.txti",
+                        "nuisance_rscan05.txt",
                         "nuisance_rscan06.txt"))
 
 design = "behavior/design.txt"
 
-model = ~ onsetTime ~ hrf(imageName) | block
+model = onsetTime ~ hrf(imageName) | block
+
+output_dir = "glm_out"
+
+polort = 4
+
+mask = "epi/global_mask.nii"
+
                       
