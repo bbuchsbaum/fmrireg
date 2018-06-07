9228f45d948c61be0f480b90c08fb7abe94e0055
Bradley Buchsbaum <bbuchsbaum@Donacobius-9.local>
2018-06-07 13:10:56 -0400

    docs

--- R/sampling_frame.R
+++ R/sampling_frame.R
@@ -24,7 +24,7 @@ sampling_frame <- function(blocklens, TR, start_time=TR/2, precision=.1) {
   assert_that(all(blocklens > 0))
   
   blockids <- rep(1:length(blocklens), blocklens)
-  scan_time <- unlist(lapply(1:length(blocklens), function(i) seq(start_time, by=TR, length.out=blocklens[i])))
+  #scan_time <- unlist(lapply(1:length(blocklens), function(i) seq(start_time, by=TR, length.out=blocklens[i])))
   ret <- list(blocklens=blocklens,
               TR=TR,
               start_time=start_time,
@@ -48,7 +48,12 @@ samples.sampling_frame <- function(x, blockids=NULL, global=FALSE) {
     }))
   } else {
     unlist(lapply(blockids, function(b) {
-      start <- if (b > 1) sum(x$blocklens[1:(b-1)])*x$TR + x$start_time else x$start_time
+      start <- if (b > 1) {
+        sum(x$blocklens[1:(b-1)])*x$TR + x$start_time 
+      } else {
+        x$start_time
+      }
+      
       seq(start, by=x$TR, length.out=x$blocklens[b])
     }))
   }
