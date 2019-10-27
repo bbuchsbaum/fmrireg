71b840e6d25af80ad57a6ffaed91f9ffcbe17096
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2019-10-27 10:23:08 -0400

    tests

--- tests/testthat/test_betas.R
+++ tests/testthat/test_betas.R
@@ -40,3 +40,36 @@ test_that("can run a beta estimation", {
  expect_true(!is.null(ret4))
   
 })
+
+test_that("can run a beta estimation with multiple basis functions", {
+  
+  facedes$frun <- factor(facedes$run)
+  facedes$constant <- factor(rep(1, nrow(facedes)))
+  
+  scans <- lapply(1:length(unique(facedes$run)), function(i) {
+    arr <- array(rnorm(10*10*10*300), c(10,10,10, 300))
+    bspace <- neuroim2::NeuroSpace(dim=c(10,10,10,300))
+    neuroim2::NeuroVec(arr, bspace)
+  })
+  
+  mask <- neuroim2::LogicalNeuroVol(array(rnorm(10*10*10), c(10,10,10)) > 0, neuroim2::NeuroSpace(dim=c(10,10,10)))
+  
+  #scans <- list.files("test_data/images_study/epi/", "rscan0.*nii", full.names=TRUE)
+  dset <- fmri_mem_dataset(scans=scans, 
+                           mask=mask, 
+                           TR=1.5, 
+                           event_table=facedes)
+  
+  hrfbasis <- do.call(gen_hrf_set, lapply(0:12, function(i) {
+    gen_hrf(hrf_gaussian, lag=i,width=.01)
+  }))
+  
+  
+  est <- estimate_betas(dset, fixed = onset ~ hrf(constant, basis=hrfbasis, durations=0),
+                                  ran = onset ~ trialwise(basis=hrfbasis, durations=0), block = ~ run, 
+                                  method="pls",ncomp=3)
+  
+  
+
+  
+})
