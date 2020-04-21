896a0eee28ef490179a15ce27aa9672cdcbc0f6c
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-04-21 10:29:36 -0400

    hrf_smoothing_kernel

--- R/hrf_smoothing_kernel.R
+++ R/hrf_smoothing_kernel.R
@@ -1,4 +1,12 @@
-hrf_smoothing_kernel <- function(len, TR=2) {
+
+#' compute an hrf smoothing kernel
+#' 
+#' @param len the number of scans
+#' @param TR the repetition time
+#' @param basis the hemodynamic response function
+#' @importFrom proxy simil
+#' @export
+hrf_smoothing_kernel <- function(len, TR=2, basis="spmg1") {
   sframe <- sampling_frame(len,TR)
   onsets <- samples(sframe)
   dfx <- data.frame(onsets=samples(sframe), block=rep(1,len))
@@ -6,5 +14,18 @@ hrf_smoothing_kernel <- function(len, TR=2) {
                     data=dfx, sampling_frame=sframe)
   
   dmat <- as.matrix(design_matrix(em))
-  cov(dmat)
+  as.matrix(proxy::simil(t(dmat), "cosine"))
+}
+
+
+#' @examples 
+#' 
+#' onsets <- sort(runif(25, 0, 200))
+#' fac <- factor(sample(letters[1:4], length(onsets), replace=TRUE))
+#' sframe <- sampling_frame(200, TR=1)
+#' des <- data.frame(onsets=onsets, fac=fac, constant = factor(rep(1, length(fac))), block=rep(1, length(fac)))
+#' emod <- event_model(onsets ~ hrf(constant) + hrf(fac), data=des, sampling_frame=sframe, block = ~ block)
+#' dmat <- design_matrix(emod)
+design_kernel <- function(dmat, deriv=FALSE) {
+  dd <- rbind(rep(0, ncol(dmat)), apply(dmat, 2, diff))
 }
