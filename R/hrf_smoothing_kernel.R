b6565f0a794a34aecb975257b4b961fdf386f34f
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-04-21 11:22:06 -0400

    smoothing kernel

--- R/hrf_smoothing_kernel.R
+++ R/hrf_smoothing_kernel.R
@@ -7,14 +7,18 @@
 #' @importFrom proxy simil
 #' @export
 hrf_smoothing_kernel <- function(len, TR=2, basis="spmg1") {
-  sframe <- sampling_frame(len,TR)
+  buffer <- 6
+  sframe <- sampling_frame(len+buffer,TR)
   onsets <- samples(sframe)
-  dfx <- data.frame(onsets=samples(sframe), block=rep(1,len))
+  dfx <- data.frame(onsets=samples(sframe), block=rep(1,len+buffer))
   em <- event_model(onsets ~ trialwise(basis="spmg1"), block=~ block, 
                     data=dfx, sampling_frame=sframe)
   
   dmat <- as.matrix(design_matrix(em))
-  as.matrix(proxy::simil(t(dmat), "cosine"))
+  #m <- as.matrix(proxy::simil(t(dmat), "cosine"))
+  m <- dmat %*% t(dmat)
+  ret <- m[(buffer/2):(len+buffer/2),(buffer/2):(len+buffer/2)]
+  ret
 }
 
 
