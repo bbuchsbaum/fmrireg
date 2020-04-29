7c3b754d0b378578e16b8d53b2af7e7a08c35d29
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-04-29 16:08:23 -0400

    chunks arg

--- R/fmri_latent_lm.R
+++ R/fmri_latent_lm.R
@@ -1,12 +1,13 @@
 
 #' @export
 fmri_latent_lm <- function(formula, block, baseline_model=NULL, dataset, 
-                    durations, drop_empty=TRUE, contrasts=NULL, robust=FALSE) {
+                    durations, drop_empty=TRUE, contrasts=NULL, robust=FALSE,
+                    strategy="chunkwise", nchunks=1) {
   
   
   assert_that(inherits(dataset, "latent_dataset"))
   result <- fmri_lm(formula, block, baseline_model=baseline_model, dataset, 
-          durations, drop_empty=drop_empty, contrasts=contrasts, robust=robust)
+          durations, drop_empty=drop_empty, contrasts=contrasts, robust=robust, nchunks=nchunks)
   result$dataset <- dataset
   class(result) <- c("fmri_latent_lm", class(result))
   result
