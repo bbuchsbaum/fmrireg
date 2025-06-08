bd305c2bcded60bddd04e9a66bc39900ccd5518c
bbuchsbaum <bbuchsbaum@example.com>
2025-06-08 16:01:40 -0400

    remove cruft

--- fmrireg.Rcheck/00_pkg_src/fmrireg/tests/testthat/test_afni_hrf_aliases.R
+++
@@ -1,9 +0,0 @@
-context("afni hrf aliases")
-
-test_that("afni_hrf accepts sin/gam aliases", {
-  spec_sin <- afni_hrf(x, basis = "sin")
-  spec_gam <- afni_hrf(x, basis = "gam")
-
-  expect_equal(attr(spec_sin$hrf, "name"), "SIN")
-  expect_equal(attr(spec_gam$hrf, "name"), "GAM")
-})
--- fmrireg.Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(fmrireg)
-Sys.setenv(R_TESTS="")
-test_check("fmrireg")
